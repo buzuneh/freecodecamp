61444eef2186eb8d0a25b70df3c8c41e4c8ee94c
buzuneh <buze.hj21@gmail.com>
2024-07-06 20:02:27 +0300

    add type of button

--- JavaScript-Algorithms-DataStructures/PalindromeChecker/script.js
+++ JavaScript-Algorithms-DataStructures/PalindromeChecker/script.js
@@ -9,16 +9,18 @@ const chen=ster2.replace(ster,"");
 if(!ster2){
   alert("Please input a value");
 }else if(ster1.test(chen) && chen.split("").reverse().join("") == chen){
-resultElement.textContent = ster2 +" is a palindrome";
+resultElement.innerHTML = ster2 +" is a palindrome";
 }else if(ster1.exec(ster2)){
-resultElement.textContent = ster2 +" is a palindrome";  
+resultElement.innerHTML = ster2 +" is a palindrome";  
 }else if(chen.split("").join("").toLowerCase()===chen.split("").reverse().join("").toLowerCase()){
-  resultElement.textContent = ster2 +" is a palindrome";  
+  resultElement.innerHTML = ster2 +" is a palindrome";  
 }else{
-  resultElement.textContent = ster2 +" is not a palindrome";  
+  resultElement.innerHTML= ster2 +" is not a palindrome";  
 }
 return chen;
 }
 checkElement.addEventListener("click",()=>{
+
   checkPal(inputElement.value);
+inputElement.value="";
 });
